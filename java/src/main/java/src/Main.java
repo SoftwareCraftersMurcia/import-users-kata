cd614ac72ebffaa140eadffb10cbca0f3652bece
Xiphereal <lozanojalex@gmail.com>
2023-07-21 19:13:56 +0200

    Movido print por pantalla a clase específica

--- java/src/main/java/src/Main.java
+++ java/src/main/java/src/Main.java
@@ -9,24 +9,9 @@ public class Main {
     List<UserProvider> userProviders = List.of(new FileUserProvider(), new UrlUserProvider());
     List<String[]> users = userProviders.stream().flatMap(p -> p.getUsers().stream()).toList();
 
-    /**
-     * users ArrayList<id: number,
-     *       email: string
-     *       first_name: string
-     *       last_name: string>
-     */
 
-    // Print users
-    System.out.println("*********************************************************************************");
-    System.out.println("* ID\t\t\t* COUNTRY\t\t* NAME\t\t\t\t* EMAIL\t\t\t\t\t\t*");
-    System.out.println("*********************************************************************************");
-    for (String[] item : users) {
-      System.out.println(String.format("* %s\t* %s\t\t* %s\t\t* %s\t*", item[0], item[3], item[2], item[5]));
-    }
-    System.out.println("*********************************************************************************");
-    System.out.println(users.size() + " users in total!");
+
+    Printer.extracted(users);
   }
-
-
 }
 
---
+++ java/src/main/java/src/Printer.java
@@ -0,0 +1,24 @@
+package src;
+
+import java.util.List;
+
+public class Printer {
+
+  /**
+   * users ArrayList<id: number,
+   *       email: string
+   *       first_name: string
+   *       last_name: string>
+   */
+  static void extracted(List<String[]> users) {
+    // Print users
+    System.out.println("*********************************************************************************");
+    System.out.println("* ID\t\t\t* COUNTRY\t\t* NAME\t\t\t\t* EMAIL\t\t\t\t\t\t*");
+    System.out.println("*********************************************************************************");
+    for (String[] item : users) {
+      System.out.println(String.format("* %s\t* %s\t\t* %s\t\t* %s\t*", item[0], item[3], item[2], item[5]));
+    }
+    System.out.println("*********************************************************************************");
+    System.out.println(users.size() + " users in total!");
+  }
+}
