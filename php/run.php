2a7fa9beba4d7ee81bbe7c6068441a6f51c0caa3
Chemaclass <chemaclass@outlook.es>
2023-07-21 19:31:29 +0200

    Extract userListWeb()

--- php/run.php
+++ php/run.php
@@ -13,43 +13,50 @@ function parseCSVFile(): array
     return $userList;
 }
 
-$userList = parseCSVFile();
+$userListCsv = parseCSVFile();
 
 # Parse URL content
 const USER_URL = 'https://randomuser.me/api/?inc=gender,name,email,location&results=5&seed=a9b25cd955e2037h';
-$web_provider = json_decode(file_get_contents(USER_URL))->results;
 
-$b = [];
-$i = 100000000000;
-foreach ($web_provider as $item) {
-    $i++;
-    if ($item instanceof stdClass) {
-        $b[] = [
-            $i, // id
-            $item->gender,
-            $item->name->first . ' ' . $item->name->last,
-            $item->location->country,
-            $item->location->postcode,
-            $item->email,
-            (new Datetime('now'))->format('Y') // birhtday
-        ];
+function userListWeb(): array
+{
+    $list = json_decode(file_get_contents(USER_URL))->results;
+
+    $b = [];
+    $i = 100000000000;
+    foreach ($list as $item) {
+        $i++;
+        if ($item instanceof stdClass) {
+            $b[] = [
+                $i, // id
+                $item->gender,
+                $item->name->first . ' ' . $item->name->last,
+                $item->location->country,
+                $item->location->postcode,
+                $item->email,
+                (new Datetime('now'))->format('Y') // birhtday
+            ];
+        }
     }
+    return $b;
 }
 
+$userListWeb = userListWeb();
+
 /**
- * @param $providers [ id -> number,
+ * @param $userList [ id -> number,
  *                   email -> string
  *                   first_name -> string
  *                   last_name -> string ] array
  */
-$providers = array_merge($userList, $b); # merge arrays
+$userList = array_merge($userListCsv, $userListWeb);
 
 # Print users
 echo "*********************************************************************************" . PHP_EOL;
 echo "* ID\t\t* COUNTRY\t* NAME\t\t* EMAIL\t\t\t\t*" . PHP_EOL;
 echo "*********************************************************************************" . PHP_EOL;
-foreach ($providers as $item) {
+foreach ($userList as $item) {
     echo sprintf("* %s\t* %s\t* %s\t* %s\t*", $item[0], $item[3], $item[2], $item[5]) . PHP_EOL;
 }
 echo "*********************************************************************************" . PHP_EOL;
-echo count($providers) . ' users in total!' . PHP_EOL;
+echo count($userList) . ' users in total!' . PHP_EOL;
