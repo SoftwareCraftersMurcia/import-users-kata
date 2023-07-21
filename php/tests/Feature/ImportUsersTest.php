e2db28968cc30934b68d30655f14702c4bc9e496
Chemaclass <chemaclass@outlook.es>
2023-07-21 18:37:20 +0200

    Scaffolding to test ImportUsersTest

---
+++ php/tests/Feature/ImportUsersTest.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace KataTest\Feature;
+
+use PHPUnit\Framework\TestCase;
+
+final class ImportUsersTest extends TestCase
+{
+    public function test_nothing(): void
+    {
+        self::assertEquals(1, 1);
+    }
+}
